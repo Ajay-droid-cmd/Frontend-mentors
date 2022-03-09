b3bf58f2d550ffa035fcd60bc929f8287b1a3267
Ajay-driod-cmd <ajay.t5102001@gmail.com>
2022-03-09 11:05:42 +0530

    made html css and js

---
+++ advice-generator-app-main/index.js
@@ -0,0 +1,11 @@
+let arr = {}
+const slip_id= Math.floor(Math.random() * 1001);
+async function fetchquote() {
+    const resp = await fetch(`https://api.adviceslip.com/advice`)
+    const data = await resp.json();
+    console.log(data.slip.id, data.slip.advice);
+     document.getElementById('main1').innerText=`Advice #${data.slip.id}`;
+     document.getElementById('main2').innerText= data.slip.advice;
+
+}
+fetchquote();
