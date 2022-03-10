4e96eff3e84af47f20400d53a2deabcac7956950
Ajay-driod-cmd <ajay.t5102001@gmail.com>
2022-03-10 21:12:20 +0530

    Shipped and deployed

--- advice-generator-app-main/index.js
+++ advice-generator-app-main/index.js
@@ -4,8 +4,8 @@ async function fetchquote() {
     const resp = await fetch(`https://api.adviceslip.com/advice`)
     const data = await resp.json();
     console.log(data.slip.id, data.slip.advice);
-     document.getElementById('main1').innerText=`ADVICE #${data.slip.id}`;
-     document.getElementById('main2').innerText= data.slip.advice;
+     document.getElementById('main1').innerText=`ADVICE   #${data.slip.id}`;
+     document.getElementById('main2').innerText= `“${data.slip.advice}”`;
 
 }
 fetchquote();
